a78767f09ed907823da7ca7eb106502219db6733
Tsaniyazka <103718776+Tsaniyazka@users.noreply.github.com>
2022-06-15 08:38:49 +0700

    Add files via upload

---
+++ UKL PBO/src/Laundry/Client.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laundry;
+
+import java.util.ArrayList;
+public class Client implements User{
+    
+ private ArrayList<String> namaClient = new ArrayList<String>();
+ private ArrayList<String> alamat = new ArrayList<String>();
+ private ArrayList<String> telepon = new ArrayList<String>();
+ private ArrayList<Integer> saldo = new ArrayList<Integer>();
+
+ public Client() {
+ setNama("Anastashia");
+ setAlamat("Jalan Danau Ranau G7/H6");
+ setTelepon("081576543192");
+ setSaldo(150000);
+
+ setNama("Alexander");
+ setAlamat("Jalan Danau Toba No.12");
+ setTelepon("087567342561");
+ setSaldo(290000);
+ 
+ setNama("Arabella");
+ setAlamat ("Jalan Danau Towuti No.6");
+ setTelepon("082986756842");
+ setSaldo(125000);
+ 
+ }
+ public void setSaldo(int saldo){
+ this.saldo.add(saldo);
+ }
+ public int getSaldo(int idClient){
+ return this.saldo.get(idClient);
+ }
+ 
+ public void editSaldo(int idClient, int saldo){
+ this.saldo.set(idClient, saldo);
+ }
+ public int getJmlClient(){
+ return this.saldo.size();
+ }
+ @Override
+ public void setNama(String namaClient ){
+ this.namaClient.add(namaClient);
+ }
+
+ @Override
+ public void setAlamat(String alamat ){
+ this.alamat.add(alamat);
+ }
+ @Override
+ public void setTelepon(String telepon ){
+ this.telepon.add(telepon);
+ }
+
+ @Override
+ public String getNama(int idClient){
+ return this.namaClient.get(idClient);
+ }
+
+ @Override
+ public String getAlamat(int idClient){
+ return this.alamat.get(idClient);
+ }
+
+ @Override
+ public String getTelepon(int idClient){
+ return this.telepon.get(idClient);
+ } 
+}
+
+
+//this untuk merujuk pada class itu sendiri
---
+++ UKL PBO/src/Laundry/Petugas.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laundry;
+
+import java.util.ArrayList;
+public class Petugas implements User{
+ private ArrayList<String> namaPetugas = new ArrayList<String>();
+ private ArrayList<String> alamat = new ArrayList<String>();
+ private ArrayList<String> telepon = new ArrayList<String>();
+ private ArrayList<Integer> jabatan = new ArrayList<Integer>();
+
+ public Petugas() {
+  setNama("Pak Doni");
+  setAlamat("Sawojajar 2");
+  setTelepon("081356782345");
+  setJabatan(0);
+ }
+
+ public void setJabatan(int jabatan ){
+ this.jabatan.add(jabatan);
+ }
+ public int getJabatan(int id){
+ return this.jabatan.get(id);
+ }
+ public int getJmlPetugas(){
+ return this.namaPetugas.size();
+ }
+
+ @Override
+ public void setNama(String namaPetugas ){
+ this.namaPetugas.add(namaPetugas);
+ }
+
+ @Override
+ public void setAlamat(String alamat ){
+ this.namaPetugas.add(alamat);
+ }
+ @Override
+ public void setTelepon(String telepon ){
+ this.telepon.add(telepon);
+ }
+
+ @Override
+ public String getNama(int idPetugas){
+ return this.namaPetugas.get(idPetugas);
+ }
+
+ @Override
+ public String getAlamat(int idPetugas){
+ return this.alamat.get(idPetugas);
+ }
+
+ @Override
+ public String getTelepon(int idPetugas){
+ return this.telepon.get(idPetugas);
+ }
+}
